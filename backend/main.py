c7b99f3ee0d1a6f692ba9b38ebba97c0bb574d07
SebastianStankiewicz <sebstankiewicz23@gmail.com>
2026-01-17 12:48:23 +0000

    Classes added

--- backend/main.py
+++ backend/main.py
@@ -4,6 +4,7 @@ from hyperliquid.info import Info
 from hyperliquid.utils import constants
 from flask import Flask, jsonify, request
 import os
+from User import *
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
@@ -43,16 +44,37 @@ def create_app():
     app = Flask(__name__)
     app.config["DEBUG"] = os.getenv("FLASK_DEBUG", "true").lower() == "true"
 
-    @app.route("/createAccount")
+    @app.route("/createAccount", methods=["POST"])
     def createAccount():
         #Will create a user account
         #User account will then have its wallet key pair created etc
-        return jsonify({"status": "ok", "message": "Flask API running"})
+        user = User()
+        user.createKeyPair()
+        
+        return jsonify({"status": "ok", "message": "Account created", "data": user.getWalletDetails()})
 
     @app.route("/deposit")
     def deposit():
         #Deposit via moonpay or similar as eth on the eth chain 
+
+        #Mark as validated or idk a transaction queue that will wait for this to validate OR offload this to the fron end aand only except complete TX???
         return jsonify({"status": "ok", "message": "Flask API running"})
+    
+    #This can be called via like after a TX been made 
+    @app.route("/processDeposit")
+    def processDeposit():
+        bridgeViaLifi()
+        convertEvmToSpot()
+        pass
+    
+    #Return there average pnl etc
+    @app.route("/getVaults")
+    def getVaults():
+        pass
+    
+    @app.route("/depositToVault")
+    def depositToVault():
+        pass
 
 
     return app
